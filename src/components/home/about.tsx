16a8776b41d4034babc1f8d56df54563e02c25a4
kschmelter13 <kschmelter13@gmail.com>
2023-09-19 23:07:52 -0400

    to grid

--- src/components/home/about.tsx
+++ src/components/home/about.tsx
@@ -5,28 +5,27 @@ import Image from "next/image";
 
 export default function About() {
 	return (
-		<div className="bg-white py-14 md:py-20 space-x-4 flex">
-			<div className="content space-y-8 md:space-y-0 xl:px-[15%] flex flex-col md:flex-row">
-				<div className="flex items-center justify-center w-full md:w-1/2 mb-4 md:mb-0 md:px-4">
-					{" "}
-					{/* Added md:px-4 */}
+		<div className="bg-white py-14 md:py-20">
+			<div className="content xl:px-[15%] grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-12 items-center">
+				<div className="flex justify-center">
 					<Animated>
 						<div className="max-w-[175px] md:max-w-[230px] xl:max-w-[275px]">
-							<Image src={KSLogo} alt={""}></Image>
+							<Image src={KSLogo} alt={""} />
 						</div>
 					</Animated>
 				</div>
-				<div className="flex items-center justify-center w-full md:w-1/2 space-y-6 text-black md:px-4">
-					{" "}
-					{/* Added md:px-4 */}
+				<div className="flex flex-col items-center space-y-6 text-black">
 					<Animated delay={0.25}>
 						<h3 className="text-center text-2xl md:text-3xl xl:text-4xl mb-6 lg:mb-8 font-medium">
 							Is KS Web Services the right fit?
 						</h3>
 						<p className="text-center text-[18px] xl:text-xl">
-							{
-								"Based in Ellington, CT, KS Web Services specializes in both web design and development, ensuring that every project receives personalized attention. With 3 years in the industry, we have honed our expertise in crafting successful digital solutions that authentically represent your brand and effectively engage your customers."
-							}
+							Based in Ellington, CT, KS Web Services specializes in both web
+							design and development, ensuring that every project receives
+							personalized attention. With 3 years in the industry, we have
+							honed our expertise in crafting successful digital solutions that
+							authentically represent your brand and effectively engage your
+							customers.
 						</p>
 					</Animated>
 				</div>
--- src/components/services/section.tsx
+++ src/components/services/section.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Animated from "../global/animation";
-import { FaCheck } from "react-icons/fa6"; // Importing the check icon
+import { FaCheck } from "react-icons/fa6";
 import Image from "next/image";
 import { urlForImage } from "../../../sanity/lib/image";
 
@@ -14,32 +14,25 @@ export default function Section({
 	const isEven = index % 2 === 0;
 
 	const bgColor = isEven ? "bg-gray-100" : "";
-	const textPosition = isEven ? "md:order-2 md:pl-6" : "md:pr-6";
-	const imagePosition = isEven ? "md:pr-6" : "md:order-2 md:pl-6";
-	const bgStyle = isEven
-		? "bg-gradient-to-br from-white to-gray-200"
-		: "bg-gradient-to-br from-gray-200 to-white";
-	const imageLayout = isEven ? "lg:justify-start " : "lg:justify-center ";
+	const textClass = isEven ? "md:order-2" : "md:order-1";
+	const imageClass = isEven ? "md:order-1" : "md:order-2";
 
 	return (
 		<div id={section.id} className={bgColor}>
-			<div className="content lg:px-[15%] 2xl:px-[20%] flex flex-col md:flex-row py-10 md:py-14 lg:py-20 items-start justify-between">
-				<div
-					className={`order-2 text-black w-full lg:w-full flex flex-col items-center justify-center ${textPosition}`}
-				>
+			<div className="content lg:px-[15%] 2xl:px-[20%] grid grid-cols-1 md:grid-cols-2 gap-10 md:gap-14 lg:gap-20 py-10 md:py-14 lg:py-20">
+				<div className={`${textClass} order-2`}>
 					<Animated delay={0.25}>
-						<h1 className="font-medium  text-[24px] sb:text-3xl mb-6 md:text-3xl xl:text-4xl">
+						<h1 className="font-medium text-[24px] mb-6 md:text-3xl xl:text-4xl">
 							{section?.title}
 						</h1>
-						<h2 className=" text-md sb:text-lg lg:text-xl mt-4 lg:mt-8">
+						<h2 className="text-md sb:text-lg lg:text-xl mt-4 lg:mt-8">
 							{section?.description}
 						</h2>
-
 						{/* Features list */}
-						<div className=" text-md md:text-lg justify-center">
-							<ul className="list-disc inline-block list-inside mt-6">
+						<div className="mt-6 ">
+							<ul className="list-disc inline-block list-inside">
 								{section?.features?.map((feature: any, index: any) => (
-									<li key={index} className="flex items-center mt-2">
+									<li key={index} className="flex mt-2 ">
 										<FaCheck className="text-black mr-4 w-4 h-4 flex-shrink-0" />
 										<span>{feature}</span>
 									</li>
@@ -49,16 +42,16 @@ export default function Section({
 					</Animated>
 				</div>
 
-				<div className={`h-full w-full mb-12  ${imagePosition}`}>
+				<div className={`${imageClass} order-1`}>
 					<Animated delay={0.3} yChange={50}>
-						<div className={`flex justify-center ${imageLayout}`}>
+						<div className="flex justify-center">
 							<Image
 								src={urlForImage(section?.image).url() || ""}
 								alt={""}
 								width={500}
 								height={500}
-								className="w-1/2 md:w-5/6 xl:w-2/3 2xl:w-3/5 "
-							></Image>
+								className="w-1/2 md:w-5/6 xl:w-2/3 2xl:w-3/5"
+							/>
 						</div>
 					</Animated>
 				</div>
